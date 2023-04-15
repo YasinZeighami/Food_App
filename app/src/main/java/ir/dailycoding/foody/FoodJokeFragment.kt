90ac432549c4ce68d449aff9c630ef71e4da553e
Yasin <yasin.zeighami@gmail.com>
2023-04-15 16:16:09 -0700

    init

---
+++ app/src/main/java/ir/dailycoding/foody/FoodJokeFragment.kt
@@ -0,0 +1,30 @@
+package ir.dailycoding.foody
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ir.dailycoding.foody.databinding.FragmentFoodJokeBinding
+
+
+class FoodJokeFragment : Fragment() {
+
+    private var _binding: FragmentFoodJokeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentFoodJokeBinding.inflate(inflater, container, false)
+
+
+
+
+        return binding.root
+    }
+
+
+}
---
+++ app/src/main/java/ir/dailycoding/foody/MainActivity.kt
@@ -0,0 +1,42 @@
+package ir.dailycoding.foody
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
+import ir.dailycoding.foody.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController : NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+         navController = findNavController(R.id.navHostFragment)
+
+        val appBarrConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.recipesFragment,
+                R.id.favoriteRecipesFragment,
+                R.id.foodJokeFragment
+            )
+        )
+
+        binding.bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController,appBarrConfiguration)
+
+
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
